06d5635e3132d95a0489381dc39c34e68802a525
Akshit Pandharkar <128708109+Akshit0604@users.noreply.github.com>
2023-03-24 20:16:20 +0530

    Delete Tours and Travels Project directory

--- Tours and Travels Project/goa conn.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector as db
-mydb=db.connect(host="localhost",user="root",passwd="12345",database="packages")
-cursor=mydb.cursor()
-cursor.execute("select * from goa")
-rec=cursor.fetchall()
-for r in rec:
-        print(r)
-        
-mydb.close()
